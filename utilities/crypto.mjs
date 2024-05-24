33942033056c369fb6e04a7b7ccb01a9ccbb46ba
VonAdamo <148451831+VonAdamo@users.noreply.github.com>
2024-05-24 09:48:53 +0200

    revert crypto

--- utilities/crypto.mjs
+++ utilities/crypto.mjs
@@ -1,6 +1,5 @@
 import crypto from 'crypto';
 
-export const createHash =(args) => {
-    return crypto.createHash('sha256').update(args).digest('hex');
-    //return crypto.createHash('sha256').update(args.sort().join("")).digest('hex');
+export const createHash =(...args) => {
+    return crypto.createHash('sha256').update(args.sort().join("")).digest('hex');
 };
